a75be287b536b2b684588c0a66925603a1bfb17a
Paul Kristapovich <paulkristapovich@gmail.com>
2017-10-25 14:14:47 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,7 +3,7 @@ require "pry"
 class Hash
   def keys_of(*arguments)
     match = []
-    keys = self.keys
+    keys = self.values
     keys.each do |x|
       if arguments.include?(x)
         match << x
