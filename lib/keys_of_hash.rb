64721fa94d64bbe4020eb764e1a9baec3555b6af
Paul Kristapovich <paulkristapovich@gmail.com>
2017-10-25 14:08:22 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,9 +1,8 @@
 class Hash
   def keys_of(*arguments)
     keys = self.keys
-    args = arguments.collect {|x| x}
     return (keys.each do |x|
-      if args.include?(x)
+      if argument.include?(x)
         x
       end
     end)
